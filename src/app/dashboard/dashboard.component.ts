8c56e0a8854874eda5d04ddfd91065d1e7fef1c7
Unknown <dzamero@gmail.com>
2020-01-13 13:53:17 -0500

    Fixed all auto-fix issues.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,6 +10,26 @@ import { ConsolidatedNetworth } from 'app/model/modelNetworthWeek';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+;
+;
+
+  networth: NetWorth[];
+
+  map_401k: Map<String, Array<number>>;
+  map_529: Map<String, Array<number>>;
+  map_ira: Map<String, Array<number>>;
+
+  consolidated_401k: ConsolidatedNetworth;
+  consolidated_529: ConsolidatedNetworth;
+  consolidated_ira: ConsolidatedNetworth;
+
+  max_401k_total: number;
+  max_529_total: number;
+  max_ira_total: number;
+
+  min_401k_total: number;
+  min_529_total: number;
+  min_ira_total: number;
 
   constructor(private _nws: NetworthService) { }
 
@@ -45,8 +65,7 @@ export class DashboardComponent implements OnInit {
     });
 
     seq = 0;
-  };
-
+  }
 
   startAnimationForBarChart(chart) {
     let seq2: any, delays2: any, durations2: any;
@@ -70,35 +89,32 @@ export class DashboardComponent implements OnInit {
     });
 
     seq2 = 0;
-  };
+  }
 
-  networth: NetWorth[];
-
-  
 
   getWeek(d: Date): Number {
 
-    // Create a copy of this date object  
-    var target: any = new Date(d.valueOf());
+    // Create a copy of this date object
+    const target: any = new Date(d.valueOf());
 
-    // ISO week date weeks start on monday  
-    // so correct the day number  
-    var dayNr: number = (d.getDay() + 6) % 7;
+    // ISO week date weeks start on monday
+    // so correct the day number
+    const dayNr: number = (d.getDay() + 6) % 7;
 
-    // Set the target to the thursday of this week so the  
-    // target date is in the right year  
+    // Set the target to the thursday of this week so the
+    // target date is in the right year
     target.setDate(target.getDate() - dayNr + 3);
 
-    // ISO 8601 states that week 1 is the week  
-    // with january 4th in it  
-    var jan4: any = new Date(target.getFullYear(), 0, 4);
+    // ISO 8601 states that week 1 is the week
+    // with january 4th in it
+    const jan4: any = new Date(target.getFullYear(), 0, 4);
 
-    // Number of days between target date and january 4th  
-    var dayDiff = (target - jan4) / 86400000;
+    // Number of days between target date and january 4th
+    const dayDiff = (target - jan4) / 86400000;
 
-    // Calculate week number: Week 1 (january 4th) plus the    
-    // number of weeks between target date and january 4th    
-    var weekNr = 1 + Math.ceil(dayDiff / 7);
+    // Calculate week number: Week 1 (january 4th) plus the
+    // number of weeks between target date and january 4th
+    const weekNr = 1 + Math.ceil(dayDiff / 7);
 
     return weekNr;
 
@@ -157,10 +173,10 @@ export class DashboardComponent implements OnInit {
   }
 
   fiveDaysSelected() {
-    console.log("FIVE days selected");
+    console.log('FIVE days selected');
     document.getElementById('dropdownMenu2').innerHTML = '5 DAYS';
 
-    let sortedNetworth = this.networth.sort(function (a, b) {
+    const sortedNetworth = this.networth.sort(function (a, b) {
       return a.insertDate < b.insertDate ? 1 : a.insertDate > b.insertDate ? -1 : 0
     });
 
@@ -168,10 +184,10 @@ export class DashboardComponent implements OnInit {
   }
 
   twoWeeksSelected() {
-    console.log("TWO weeks selected");
+    console.log('TWO weeks selected');
     document.getElementById('dropdownMenu2').innerHTML = '2 WEEKS';
 
-    let sortedNetworth = this.networth.sort(function (a, b) {
+    const sortedNetworth = this.networth.sort(function (a, b) {
       return a.insertDate < b.insertDate ? 1 : a.insertDate > b.insertDate ? -1 : 0
     });
 
@@ -179,15 +195,15 @@ export class DashboardComponent implements OnInit {
   }
 
   oneMonthSelected() {
-    console.log("ONE month selected");
+    console.log('ONE month selected');
     document.getElementById('dropdownMenu2').innerHTML = '1 MONTH';
   }
 
-  getChartsForDays(sortedNetworth:NetWorth[], days:number){
-    let firstFive = sortedNetworth.slice(0, days);
+  getChartsForDays(sortedNetworth: NetWorth[], days: number) {
+    const firstFive = sortedNetworth.slice(0, days);
 
     console.log(firstFive);
-    //this.convertToPlottingValues(firstFive, 5, "day");
+    // this.convertToPlottingValues(firstFive, 5, "day");
 
     this.consolidated_401k = new ConsolidatedNetworth();
     this.consolidated_529 = new ConsolidatedNetworth();
@@ -199,45 +215,33 @@ export class DashboardComponent implements OnInit {
     this.max_529_total = Math.max.apply(Math, this.consolidated_529.yAxisValues.map(function (o) { return o; }))
     this.max_ira_total = Math.max.apply(Math, this.consolidated_ira.yAxisValues.map(function (o) { return o; }))
 
-    console.log("max 401k: " + this.max_401k_total);
-    console.log("max 529: " + this.max_529_total);
-    console.log("max ira: " + this.max_ira_total);
+    console.log('max 401k: ' + this.max_401k_total);
+    console.log('max 529: ' + this.max_529_total);
+    console.log('max ira: ' + this.max_ira_total);
 
     this.min_401k_total = Math.min.apply(Math, this.consolidated_401k.yAxisValues.map(function (o) { return o; }))
     this.min_529_total = Math.min.apply(Math, this.consolidated_529.yAxisValues.map(function (o) { return o; }))
     this.min_ira_total = Math.min.apply(Math, this.consolidated_ira.yAxisValues.map(function (o) { return o; }))
 
-    console.log("min 401k: " + this.min_401k_total);
-    console.log("min 529: " + this.min_529_total);
-    console.log("min ira: " + this.min_ira_total);
+    console.log('min 401k: ' + this.min_401k_total);
+    console.log('min 529: ' + this.min_529_total);
+    console.log('min ira: ' + this.min_ira_total);
 
-    this.retrieveTestChart(this.consolidated_401k.xAxisValues, this.consolidated_401k.yAxisValues, 1000, 1000, this.min_401k_total, this.max_401k_total, "#testChart_401k");
-    this.retrieveTestChart(this.consolidated_529.xAxisValues, this.consolidated_529.yAxisValues, 1000, 1000, this.min_529_total, this.max_529_total, "#testChart_529");
-    this.retrieveTestChart(this.consolidated_ira.xAxisValues, this.consolidated_ira.yAxisValues, 1000, 1000, this.min_ira_total, this.max_ira_total, "#testChart_ira");
+    this.retrieveTestChart(this.consolidated_401k.xAxisValues, this.consolidated_401k.yAxisValues,
+       1000, 1000, this.min_401k_total, this.max_401k_total, '#testChart_401k');
+    this.retrieveTestChart(this.consolidated_529.xAxisValues, this.consolidated_529.yAxisValues,
+       1000, 1000, this.min_529_total, this.max_529_total, '#testChart_529');
+    this.retrieveTestChart(this.consolidated_ira.xAxisValues, this.consolidated_ira.yAxisValues,
+       1000, 1000, this.min_ira_total, this.max_ira_total, '#testChart_ira');
   }
 
-  map_401k: Map<String, Array<number>>;
-  map_529: Map<String, Array<number>>;
-  map_ira: Map<String, Array<number>>;
+  getPlottingForDays(givenNetworthValues: NetWorth[], result1: ConsolidatedNetworth,
+     result2: ConsolidatedNetworth, result3: ConsolidatedNetworth) {
+    const xAxis = new Array<String>();
 
-  consolidated_401k: ConsolidatedNetworth;
-  consolidated_529: ConsolidatedNetworth;
-  consolidated_ira: ConsolidatedNetworth;
-
-  max_401k_total: number;
-  max_529_total: number;
-  max_ira_total: number;
-
-  min_401k_total: number;
-  min_529_total: number;
-  min_ira_total: number;
-
-  getPlottingForDays(givenNetworthValues: NetWorth[], result1: ConsolidatedNetworth, result2: ConsolidatedNetworth, result3: ConsolidatedNetworth) {
-    let xAxis = new Array<String>();
-
-    let yAxis_401k = new Array<number>();
-    let yAxis_529 = new Array<number>();
-    let yAxis_ira = new Array<number>();
+    const yAxis_401k = new Array<number>();
+    const yAxis_529 = new Array<number>();
+    const yAxis_ira = new Array<number>();
 
     for (let i = 0; i < givenNetworthValues.length; i++) {
       xAxis.push(this.getFormattedDate(givenNetworthValues[i].insertDate));
@@ -257,14 +261,15 @@ export class DashboardComponent implements OnInit {
   }
 
   getFormattedDate(givenDate: Date) {
-    let dateString: String = new Date(givenDate).toLocaleDateString();
-    return dateString.substring(0, dateString.lastIndexOf("/"));
+    const dateString: String = new Date(givenDate).toLocaleDateString();
+    return dateString.substring(0, dateString.lastIndexOf('/'));
   }
 
-  retrieveTestChart(xAxisValues: String[], yAxisValues: number[], decrement: number, increment: number, minValue: number, maxValue: number, target: string) {
+  retrieveTestChart(xAxisValues: String[], yAxisValues: number[], decrement: number,
+     increment: number, minValue: number, maxValue: number, target: string) {
 
-    let targetForToolTip:String = target.substring(1,target.length);
-    let selectorForToolTip:string = '[data-chart-tooltip="' + targetForToolTip + '"]';
+    const targetForToolTip: String = target.substring(1, target.length);
+    const selectorForToolTip: string = '[data-chart-tooltip="' + targetForToolTip + '"]';
 
     const dataDailySalesChart: any = {
       labels: xAxisValues,
@@ -284,13 +289,13 @@ export class DashboardComponent implements OnInit {
       chartPadding: { top: 0, right: 0, bottom: 0, left: 30 },
       axisY: {
         labelInterpolationFnc: function (value: any) {
-          //console.log("The values are: "+ value + " == " + "$" + Number((value).toFixed(1)).toLocaleString());
-          return "$" + Number((value).toFixed(1)).toLocaleString();
+          // console.log("The values are: "+ value + " == " + "$" + Number((value).toFixed(1)).toLocaleString());
+          return '$' + Number((value).toFixed(1)).toLocaleString();
         }
       }
     }
 
-    var dailySalesChart = new Chartist.Line(target, dataDailySalesChart, optionsDailySalesChart)
+    const dailySalesChart = new Chartist.Line(target, dataDailySalesChart, optionsDailySalesChart)
                             // .on("draw", function (data) {
                             //   if (data.type === "point") {
                             //     data.element._node.setAttribute("title", "" + data.value.y);
